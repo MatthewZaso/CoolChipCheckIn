d03bdbe3f6a225c044f665dca1e96bcf4a19d986
MatthewZaso <matthew.zaso@intercom.io>
2020-06-09 17:44:54 -0700

    Cleaning up lodash dep import

--- src/components/search/Search.tsx
+++ src/components/search/Search.tsx
@@ -1,5 +1,5 @@
-import * as React from "react";
-import { throttle } from 'lodash';
+import * as React from 'react';
+import throttle from 'lodash.throttle';
 import './search.scss';
 
 interface IProps {
